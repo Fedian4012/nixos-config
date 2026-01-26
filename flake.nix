499c49023818aeca2eff50a6899f96401789393e
François Ruau <francois.fedian.4012@free.fr>
2026-01-26 21:06:56 +0100

    Fix syntax for outputs declaration in flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
   # -------------------
   # Outputs
   # -------------------
-  outputs = inputs@{ nixpkgs, home-manager, ... }: 
+  outputs = { nixpkgs, home-manager, ... }: 
     let
       system = "x86_64-linux";  # Architecture de la machine
     in
